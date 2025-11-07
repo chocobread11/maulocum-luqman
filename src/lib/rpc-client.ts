55f4953d1ae00caabd10f95e99a5756e06f076e0
krekz <kfitri559@gmail.com>
2025-11-07 18:05:57 +0800

    feat: enable cookie in RPC client configuration

--- src/lib/rpc-client.ts
+++ src/lib/rpc-client.ts
@@ -1,7 +1,11 @@
 import { hc, type InferResponseType } from "hono/client";
 import type { APIType } from "@/app/api/[...route]/route";
 
-export const client = hc<APIType>("/");
+export const client = hc<APIType>("/", {
+	init: {
+		credentials: "include",
+	},
+});
 export const backendApi = hc<APIType>(process.env.BETTER_AUTH_URL as string);
 
 export type JobResponse = InferResponseType<typeof backendApi.api.v2.jobs.$get>;
