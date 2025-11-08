c5264b711048727e98fd4ed85be61a87d3e37b3f
krekz <kfitri559@gmail.com>
2025-11-08 12:01:37 +0800

    refactor: restructure admin dashboard layout and remove unused data

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,22 +1,67 @@
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "../_components/admin-sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { DynamicBreadcrumb } from "@/components/dynamic-breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export default function AdminDashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const data = {
+		basic: [
+			{
+				title: "Dashboard",
+				url: "/admin/dashboard",
+			},
+			{
+				title: "Users",
+				url: "/admin/dashboard/users",
+			},
+		],
+		collapsible: [
+			{
+				title: "Doctors",
+				url: "/admin/dashboard/doctors",
+				items: [
+					{
+						title: "Verifications",
+						url: "/admin/dashboard/doctors/verifications",
+					},
+				],
+			},
+			{
+				title: "Facilities",
+				url: "/admin/dashboard/facilities",
+				items: [
+					{
+						title: "Verifications",
+						url: "/admin/dashboard/facilities/verifications",
+					},
+				],
+			},
+		],
+	};
 	return (
 		<SidebarProvider>
-			<AppSidebar variant="inset" />
+			<AppSidebar data={data} />
 			<SidebarInset>
-				<SiteHeader />
+				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+					<SidebarTrigger className="-ml-1" />
+					<Separator
+						orientation="vertical"
+						className="mr-2 data-[orientation=vertical]:h-4"
+					/>
+					<DynamicBreadcrumb />
+				</header>
+
 				<div className="flex flex-1 flex-col">
 					<div className="@container/main flex flex-1 flex-col gap-2">
-						<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-							{children}
-						</div>
+						<div className="flex flex-col gap-4 py-4">{children}</div>
 					</div>
 				</div>
 			</SidebarInset>
